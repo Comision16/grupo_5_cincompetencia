4dc526a913662f6d652d54db694bfb03778461f7
Milagros <miilyailen97@gmail.com>
2024-01-24 02:39:03 -0300

    inicio de implementacion de autenticacion de usuario

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,13 +3,14 @@ const { login, register, processRegister, process_login, profile } = require('..
 const userRegisterValidator = require('../../validations/user-register-validator');
 const loginValidation = require('./../../validations/user-login-validator');
 const checkUserLogin = require('../middlewares/checkUserLogin');
+const checkAuthUser = require('../middlewares/checkAuthUser');
 const router = express.Router();
 
 /* /usuarios */
 router
-.get('/ingreso',login)
-.post('/ingreso',loginValidation,process_login)
-.get('/registro',register)
+.get('/ingreso',checkAuthUser, login)
+.post('/ingreso', loginValidation,process_login)
+.get('/registro',checkAuthUser, register)
 .post('/registro',userRegisterValidator, processRegister)
 .get('/perfil',checkUserLogin, profile)
 
